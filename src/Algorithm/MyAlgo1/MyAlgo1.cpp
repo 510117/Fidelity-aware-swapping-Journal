1737c2b96d0bf76ef9262bbbab4ae39e8a15434f
qq11123334 <qq11123334@csie.io>
2024-01-29 15:00:03 +0800

    change myalgo1 round

--- src/Algorithm/MyAlgo1/MyAlgo1.cpp
+++ src/Algorithm/MyAlgo1/MyAlgo1.cpp
@@ -191,7 +191,7 @@ void MyAlgo1::run() {
     // alpha(v) = alpha(v)(1 + eps(q / ahpla(v))
     // beta(v, t) = beta(v, t)(1 + eps(q / beta(v, t))
 
-    int round = 2;
+    int round = 1;
     while(round-- && !requests.empty()) {
         variable_initialize();
         while(obj < 1.0) {
