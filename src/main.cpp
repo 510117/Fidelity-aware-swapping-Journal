bc32ca0f85a5946bff5287d7560e8cbedab382b0
qq11123334 <qq11123334@csie.io>
2024-01-29 17:25:03 +0800

    change para

--- src/main.cpp
+++ src/main.cpp
@@ -70,8 +70,8 @@ int main(){
     default_setting["avg_memory"] = 7;
     default_setting["tao"] = 0.5;
     default_setting["path_length"] = 5;
-    default_setting["min_fidelity"] = 0.8;
-    default_setting["max_fidelity"] = 0.95;
+    default_setting["min_fidelity"] = 1;
+    default_setting["max_fidelity"] = 1;
     default_setting["swap_prob"] = 0.9;
     default_setting["fidelity_threshold"] = 0.5;
 
