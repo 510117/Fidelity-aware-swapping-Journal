8e3823a32317b67df1f78c4b8ee1dacc2204a598
qq11123334 <qq11123334@csie.io>
2024-02-04 00:51:52 +0800

    change para

--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,7 @@ int main(){
     default_setting["request_cnt"] = 30;
     default_setting["area_alpha"] = 0.0005;
     default_setting["time_limit"] = 7;
-    default_setting["avg_memory"] = 9;
+    default_setting["avg_memory"] = 7;
     default_setting["tao"] = 0.5;
     default_setting["path_length"] = 7;
     default_setting["min_fidelity"] = 0.7;
