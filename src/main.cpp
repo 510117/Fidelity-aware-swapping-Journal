552f72dd51e9e855c6733f63aaeca863528e560a
qq11123334 <qq11123334@csie.io>
2024-02-04 00:56:21 +0800

    change para

--- src/main.cpp
+++ src/main.cpp
@@ -71,11 +71,11 @@ int main(){
     default_setting["num_nodes"] = 100;
     default_setting["request_cnt"] = 30;
     default_setting["area_alpha"] = 0.0005;
-    default_setting["time_limit"] = 7;
-    default_setting["avg_memory"] = 7;
+    default_setting["time_limit"] = 6;
+    default_setting["avg_memory"] = 6;
     default_setting["tao"] = 0.5;
-    default_setting["path_length"] = 7;
-    default_setting["min_fidelity"] = 0.7;
+    default_setting["path_length"] = 10;
+    default_setting["min_fidelity"] = 0.8;
     default_setting["max_fidelity"] = 0.98;
     default_setting["swap_prob"] = 0.9;
     default_setting["fidelity_threshold"] = 0.5;
