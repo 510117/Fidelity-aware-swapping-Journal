c6da144c2b31c559db4c59fcf238317d4f3c261c
qq11123334 <qq11123334@csie.io>
2024-02-05 20:01:16 +0800

    compile REPS

--- src/Network/PathMethod/REPS/REPS.h
+++ src/Network/PathMethod/REPS/REPS.h
@@ -2,12 +2,14 @@
 #define __REPS_H
 
 #include "../PathMethodBase/PathMethod.h"
-
+#include <set>
+#include <cmath>
+#include "gurobi_c++.h"
 using namespace std;
 
 class REPS : public PathMethod {
     void PFT_LP(vector<double> &t_plum, vector<map<pair<int, int>, double>> &f_plum);
-    pair<vector<int>, double> REPS::dijkstra(int src, int dst, map<pair<int, int>, double>&f_plum_i);
+    pair<Path, double> dijkstra(int src, int dst, map<pair<int, int>, double>&f_plum_i);
     void update_f_plum(Path path, double width, map<pair<int, int>, double>&f_plum_i);
 public:
     REPS();
